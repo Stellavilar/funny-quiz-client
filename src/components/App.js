b80397d44c9684f74da742c57b13d9f57554d671
Stella Vilar <vilar.stella@gmail.com>
2021-03-13 13:49:01 +0100

    setting up quizzes by sub-categories

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import '../styles/index.scss';
 
 import Page404 from './Page404';
 import Dashboard from './Dashboard';
+import SubCatQuiz from './SubCatQuiz';
 
 
 function App () {
@@ -12,6 +13,9 @@ function App () {
       <Switch>
         <Route exact path='/'>
           <Dashboard />
+        </Route>
+        <Route exact path='/classifiedQuiz/:id'> 
+            <SubCatQuiz />
         </Route>
         <Route>
           <Page404 />
--- src/components/SubCatQuiz.js
+++ src/components/SubCatQuiz.js
@@ -1,9 +1,97 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import { useParams } from 'react-router';
+import { Segment, Header, Form, Checkbox, Dimmer, Loader } from 'semantic-ui-react';
 
 function SubCatQuiz () {
+    const history = useHistory();
+    /**Check answer */
+    const [ answ, setAnsw ] = useState([]);
+    /**Show anecdote */
+    const [ showWiki, setShowWiki ] = useState([]);
+    /**Handle checkbox */
+    const [ state, setState] = useState({});
+    const handleChange = (e, { value }) => setState({value});
+    /**Count score */
+    const [ count, setCount ] = useState(0);
+    /**Loader */
+    const [ loading, setLoading ] = useState(false);
+
+    /**Handle submit quiz form */
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let paragraph = e.target.lastElementChild;
+        e.target.reset();
+        const button = e.target.children[3];
+        button.nextElementSibling.remove();
+
+        if(!state.value){
+            paragraph.textContent = ' * Vous n\'avez pas coché de réponse !';
+            paragraph.className = 'noAnswer';
+        }
+        else if (state.value === answ){
+            paragraph.textContent = 'Bonne réponse :  ' + showWiki;
+            paragraph.className = 'goodAnswer';
+            setCount(count + 1);
+        }else{
+            paragraph.textContent = 'Mauvaise réponse...';
+            paragraph.className = 'badAnswer';         
+        }
+        
+    };
+
+    /**Handle click on logo */
+    const handleClick = () => {
+        history.goBack();
+    };
+
+    /**Get quizzes by subcategory */
+    let {id} = useParams();
+    const [ subCat, setSubCat ] =useState([]);
+    const subCategories = () => {
+        axios.get
+        (`quizzes/subcategory/${id}`)
+        .then((res) => {
+            setSubCat(res.data);
+            setLoading(true);      
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    };
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(() => { subCategories() }, [])
+
+    /**Quiz + answers */
+    const getQuiz = subCat.map((subcateg) =>
+        <Segment key={subcateg.id}>
+            <li>{subcateg.question}</li>
+        <Form
+        onSubmit={handleSubmit}
+        >
+            <Checkbox radio label={subcateg.prop2} value={subcateg.prop2} checked={state.value === subcateg.prop2} onChange={handleChange} onClick={e => setAnsw(subcateg.answer.title)}/>
+            <Checkbox radio label={subcateg.prop3} value={subcateg.prop3} checked={state.value === subcateg.prop3} onChange={handleChange} onClick={e => setAnsw(subcateg.answer.title)}/>
+            <Checkbox radio label={subcateg.prop1} value={subcateg.prop1} checked={state.value === subcateg.prop1} onChange={handleChange} onClick={e => setAnsw(subcateg.answer.title)}/>
+            <Checkbox radio label={subcateg.prop4} value={subcateg.prop4} checked={state.value === subcateg.prop4} onChange={handleChange} onClick={e => setAnsw(subcateg.answer.title)}/>
+            <Form.Button color='grey' type='submit' onClick={e => setShowWiki(subcateg.anecdote)} >Valider</Form.Button>
+            <p></p>
+        </Form>
+    </Segment> 
+    );
+
     return (
         <div className="quiz">
-            test
+            <div className="main-quiz">
+            <p className="arrow" onClick={handleClick}>&#8678; Retour en arrière</p>
+                <Header as='h2'>Questions</Header>
+                {loading ? [] :  <Dimmer active inverted><Loader inverted /></Dimmer> }
+                {getQuiz}
+            </div>
+            <div className="counter">
+                <p>Vous avez {count} points</p>
+            </div>
         </div>
     );
 };
